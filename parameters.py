b20b5e0b354cc4a4f1d825c38dda1351de194ca7
Yinan Zhu <welltempered.ivan@gmail.com>
2018-04-01 21:56:38 -0500

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -1,3 +1,4 @@
-training_sample_size=30000 # the upper bound of numer of lines you want your program to read
+training_sample_size=30000 #numer of lines you want your program to read in training file
+test_size=2000 # number of lines you want your program to read in test file
 test_file='test.txt' #test file name here
 training_file='train.txt' #training file name here
