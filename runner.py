ef0babe71dbe701f6b1ddc77f13c1df3142a8d26
Yinan Zhu <welltempered.ivan@gmail.com>
2018-04-01 21:50:29 -0500

    Update runner.py

--- runner.py
+++ runner.py
@@ -8,14 +8,10 @@ def predict_review(trie):
     review_typed = ''
     while True:
         next_chars = stdin.readline().rstrip('\r\n')
-        if len(next_chars) == 0: # the grader will pass a blank line when done typing this review.
+        if len(next_chars) == 0:
             return
         review_typed += next_chars
-        ### BEGIN CHANGES HERE
         preds=f.predict(review_typed,trie)
-        #print(next_chars,file=stderr)
-        #print(preds,file=stderr)
-        #### END USER CHANGES HERE 
         try:
             print('{}{}{}{}{}'.format(preds[0], sep, preds[1], sep, preds[2]))
             stdout.flush()
@@ -23,14 +19,11 @@ def predict_review(trie):
             print(' {} {} '.format(sep, sep)) # in the event that anything weird happens, pass three blank predictions
             stdout.flush()
 
-trie=f.train(para.training_sample_size,para.review)
+trie=f.train(para.training_sample_size,para.training_file)
 
 while True:
     try:
         print('Predicting review.', file=stderr)
-        metadata = stdin.readline().rstrip('\r\n').split(',')
-        product_id, user_id, unix_date, rating = metadata
-        ### TODO: you may wish to modify arguments to predict_review.
         predict_review(trie)
     except EOFError:
         sys.exit(0)
